67c05e8742d2099b909645c1a89360ac8d1afda6
YeniferQ <yenifersorj@gmail.com>
2022-11-29 17:35:23 -0600

    decision1

--- Dia de mañana/Dia de mañana.cpp
+++ Dia de mañana/Dia de mañana.cpp
@@ -10,4 +10,7 @@ int main()
 	scanf_s("%i", &Dia1);
 	scanf_s("%i", &Mes1);
 	scanf_s("%i", &anio);
+
+	if (!((Dia1 >= 4 && Dia1 < 15) && Mes1 == 10 && anio == 1582)) {
+		if (Mes1 == 1 && Dia1 < 31) { printf("%2i %2i %4i", Dia1 + 1, Mes1, anio); }
 }
